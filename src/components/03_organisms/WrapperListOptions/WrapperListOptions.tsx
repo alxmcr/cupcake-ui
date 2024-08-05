527c7d753f36e37ab1633969568cb7d00bbbf72d
Alejandro Coca <alxmcr@outlook.com>
2024-08-05 00:34:15 -0400

    wip

--- src/components/03_organisms/WrapperListOptions/WrapperListOptions.tsx
+++ src/components/03_organisms/WrapperListOptions/WrapperListOptions.tsx
@@ -5,9 +5,12 @@ type Props = {
   options: OptionData[];
 };
 
+//  box-shadow: 0 4px 8px rgba(64, 67, 68, 24%);
+// shadow-[0px_4px_8px_0px_rgba(64, 67, 68, 24%)]
+
 export default function WrapperListOptions({ options = [] }: Props) {
   return (
-    <div>
+    <div className="u-list-options-shadow h-[114px] w-[268px] rounded-lg p-2">
       <ListOptionsUser options={options} />
     </div>
   );
