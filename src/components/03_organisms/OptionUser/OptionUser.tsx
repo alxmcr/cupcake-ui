3a35880ee5cc0e1c5722624e1c1ed4693917014b
Alejandro Coca <alxmcr@outlook.com>
2024-08-05 02:32:50 -0400

    wip

--- src/components/03_organisms/OptionUser/OptionUser.tsx
+++ src/components/03_organisms/OptionUser/OptionUser.tsx
@@ -9,15 +9,15 @@ type Props = {
 };
 
 export default function OptionUser({ id = '', text = '', selected = false, setOptionSelectedId }: Props) {
-  const onClick = () => {
-    setOptionSelectedId(id);
+  const onClick = (idSelected = '') => {
+    setOptionSelectedId(idSelected);
   };
 
   return (
     <li
       id={id}
       className="flex h-[46px] w-[252px] cursor-pointer list-none items-center justify-between gap-1 rounded-lg p-4"
-      onClick={onClick}
+      onClick={() => onClick(id)}
     >
       <InfoUserProfile selected={selected} text={text} />
       {selected ? <AppIcon16x16 iconName="icon-checkmark" className="text-cyan-700" /> : null}
