f09ac42ca41e08b940b74ccc18bdbf34f86c58da
Alejandro Coca <alxmcr@outlook.com>
2024-08-04 20:01:15 -0400

    wip

---
+++ src/components/atoms/Input/Input.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Input from './Input';
+
+const meta = {
+  title: 'Atoms/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    type: { control: 'text' },
+    id: { control: 'text' },
+    name: { control: 'text' },
+    placeholder: { control: 'text' },
+    readOnly: { control: 'boolean' },
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Text: Story = {
+  args: {
+    type: 'text',
+    id: 'input-text',
+    name: 'user-name',
+    placeholder: 'Elige un usuario',
+    readOnly: false,
+  },
+};
