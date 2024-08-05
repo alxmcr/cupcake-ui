3f8b5a1e0284abf288bf06e98731ad329fc4df5f
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-08-05 02:11:05 -0400

    Feat/form field (#6)

---
+++ src/components/03_organisms/ControlComboBox/ControlComboBox.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ControlComboBox from './ControlComboBox';
+
+const meta = {
+  title: 'Organisms/ControlComboBox',
+  component: ControlComboBox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    inputType: { control: 'text' },
+    id: { control: 'text' },
+    name: { control: 'text' },
+    readOnly: { control: 'boolean' },
+    labelText: { control: 'text' },
+  },
+} satisfies Meta<typeof ControlComboBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Text: Story = {
+  args: {
+    inputType: 'text',
+    id: 'ControlComboBox-text',
+    name: 'user-name',
+    readOnly: false,
+    labelText: 'Elige un usuario',
+  },
+};
