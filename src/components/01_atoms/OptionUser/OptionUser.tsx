c9fdddd4c557dcfb577ec97075b791da919617ed
Alejandro Coca <alxmcr@outlook.com>
2024-08-04 20:40:04 -0400

    wip

--- src/components/01_atoms/OptionUser/OptionUser.tsx
+++ src/components/01_atoms/OptionUser/OptionUser.tsx
@@ -9,18 +9,28 @@ type Props = {
 export default function OptionUser({ id = '', text = '', selected = false }: Props) {
   if (selected) {
     return (
-      <li id={id} className="list-none font-semibold text-gray-800">
-        <AppIcon16x16 iconName="icon-profile" />
-        <span>{text}</span>
+      <li
+        id={id}
+        className="flex h-[46px] w-[268px] list-none items-center justify-between gap-1 rounded-lg font-extrabold text-gray-800"
+      >
+        <div className="flex items-center justify-between gap-2">
+          <AppIcon16x16 iconName="icon-profile" />
+          <span className="font-inter">{text}</span>
+        </div>
         <AppIcon16x16 iconName="icon-checkmark" />
       </li>
     );
   }
 
   return (
-    <li id={id} className="list-none">
-      <AppIcon16x16 iconName="icon-profile" />
-      <span>{text}</span>
+    <li
+      id={id}
+      className="flex h-[46px] w-[268px] list-none items-center justify-between gap-1 rounded-lg text-gray-800"
+    >
+      <div className="flex items-center justify-between gap-2">
+        <AppIcon16x16 iconName="icon-profile" className="text-gray-500" />
+        <span className="font-inter">{text}</span>
+      </div>
     </li>
   );
 }
