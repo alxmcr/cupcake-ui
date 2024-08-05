60601c145c19cca2c101b466a042ca15e06990c2
Alejandro Coca <alxmcr@outlook.com>
2024-08-05 02:31:29 -0400

    wip

--- src/components/03_organisms/ComboBox/ComboBox.tsx
+++ src/components/03_organisms/ComboBox/ComboBox.tsx
@@ -7,13 +7,22 @@ type Props = {
 };
 
 export default function ComboBox({ options = [] }: Props) {
+  const [comboBoxText, setComboBoxText] = React.useState('Open combobox');
   const [isOpenComboBox, setIsOpenComboBox] = React.useState(false);
 
   const toggleComboBox = () => setIsOpenComboBox((prevIsOpen) => !prevIsOpen);
 
+  React.useEffect(() => {
+    if (isOpenComboBox) {
+      setComboBoxText('Close combobox');
+    } else {
+      setComboBoxText('Open combobox');
+    }
+  }, [isOpenComboBox]);
+
   return (
     <div>
-      <div onClick={toggleComboBox}>Open combobox</div>
+      <div onClick={toggleComboBox}>{comboBoxText}</div>
       {isOpenComboBox ? <WrapperListOptions options={options} /> : null}
     </div>
   );
