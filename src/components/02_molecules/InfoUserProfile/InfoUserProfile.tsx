3dc408db1030d4b686accdcd06a0d8ec3e03ba8d
Alejandro Coca <alxmcr@outlook.com>
2024-08-04 22:43:12 -0400

    wip

---
+++ src/components/02_molecules/InfoUserProfile/InfoUserProfile.tsx
@@ -0,0 +1,23 @@
+import AppIcon16x16 from '../../00_icons/Icon16x16/Icon16x16';
+import { LabelOption } from '../../01_atoms/LabelOption';
+
+type Props = {
+  text: string;
+  selected: boolean;
+};
+
+export default function InfoUserProfile({ text = '', selected = false }: Props) {
+  if (selected) {
+    <div className="flex items-center justify-between gap-2 text-gray-800">
+      <AppIcon16x16 iconName="icon-profile" />
+      <LabelOption text={text} />
+    </div>;
+  }
+
+  return (
+    <div className="flex items-center justify-between gap-2 text-gray-500">
+      <AppIcon16x16 iconName="icon-profile" />
+      <LabelOption text={text} />
+    </div>
+  );
+}
