bb3aa03688a6fe8aeca9dc349f64d3c2a7c7ae36
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-08-06 00:57:11 -0400

    fix/colors-combobox-control (#23)

--- src/components/03_organisms/ControlComboBox/ControlComboBox.tsx
+++ src/components/03_organisms/ControlComboBox/ControlComboBox.tsx
@@ -53,7 +53,7 @@ export default function ControlComboBox({
         >
           {labelText}
         </label>
-        <div className="absolute right-4 top-[14px] z-20 size-[20px] duration-300 peer-focus:peer-placeholder-shown:rotate-180">
+        <div className="absolute right-4 top-[14px] z-20 size-[20px] text-gray-500 duration-300 peer-focus:peer-placeholder-shown:rotate-180 peer-focus:peer-placeholder-shown:text-gray-600">
           <Icon20x20 iconName="icon-arrow-down" />
         </div>
       </div>
