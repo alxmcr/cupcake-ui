1825ad7240a0baf5c5baae1a704252e079473627
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-08-06 01:18:35 -0400

    fix/dimensions-combobox-controller (#25)

--- src/components/03_organisms/ControlComboBox/ControlComboBox.tsx
+++ src/components/03_organisms/ControlComboBox/ControlComboBox.tsx
@@ -32,14 +32,14 @@ export default function ControlComboBox({
   const onClick = () => onClickControlComboBox(inputTextRef);
 
   return (
-    <div className="w-[268px] md:w-[632px] lg:w-[996px]" onClick={onClick}>
-      <div className="hover:u-list-options-shadow relative rounded-lg">
+    <div className="flex w-[268px] flex-col gap-1 md:w-[632px] lg:w-[996px]" onClick={onClick}>
+      <div className="hover:u-list-options-shadow relative  max-h-[58px]  rounded-lg">
         <input
           ref={inputTextRef}
           type={inputType}
           id={id}
           name={name}
-          className="peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-cyan-900 focus:outline-none focus:ring-0 "
+          className="peer block h-[38px] w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-cyan-900 focus:outline-none focus:ring-0 "
           placeholder=" "
           readOnly={readOnly}
           autoComplete="off"
@@ -49,11 +49,11 @@ export default function ControlComboBox({
         />
         <label
           htmlFor={id}
-          className="absolute start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-cyan-900 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 "
+          className="absolute start-1 top-2 origin-[0] -translate-y-4 scale-75 bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-cyan-900 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 "
         >
           {labelText}
         </label>
-        <div className="absolute right-4 top-[14px] z-20 size-[20px] text-gray-500 duration-300 peer-focus:peer-placeholder-shown:rotate-180 peer-focus:peer-placeholder-shown:text-gray-600">
+        <div className="absolute right-4 top-[9px] size-[20px] text-gray-500 duration-300 peer-focus:peer-placeholder-shown:rotate-180 peer-focus:peer-placeholder-shown:text-gray-600">
           <Icon20x20 iconName="icon-arrow-down" />
         </div>
       </div>
--- src/components/03_organisms/ListOptionsUser/ListOptionsUser.tsx
+++ src/components/03_organisms/ListOptionsUser/ListOptionsUser.tsx
@@ -10,14 +10,14 @@ type Props = {
 export default function ListOptionsUser({ options = [], idOptionSelected = '', onSelectOption }: Props) {
   if (options.length === 0) {
     return (
-      <div className="flex h-[46px] w-full items-center justify-center md:w-[616px] lg:w-[970px]">
+      <div className="flex h-[46px] w-full items-center justify-center bg-white md:w-[616px] lg:w-[970px]">
         <p>No users</p>
       </div>
     );
   }
 
   return (
-    <ol className="flex max-h-[110px] w-full flex-col gap-1 overflow-y-auto overflow-x-hidden   md:w-[616px] lg:w-[980px]">
+    <ol className="flex max-h-[110px] w-full flex-col gap-1 overflow-y-auto overflow-x-hidden bg-white md:w-[616px] lg:w-[980px]">
       {options.map((option) => (
         <OptionUser
           key={option.id}
