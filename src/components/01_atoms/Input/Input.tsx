6a56fc4f458d77970de022d1825e4cd8c2ac5894
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-08-07 22:47:42 -0400

    testing/visual-testing (#50)

--- src/components/01_atoms/Input/Input.tsx
+++ src/components/01_atoms/Input/Input.tsx
@@ -6,7 +6,7 @@ const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
   return (
     <input
       ref={ref}
-      className="peer block h-[38px] w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-cyan-900 focus:outline-none focus:ring-0 "
+      className="peer block h-[48px] w-full appearance-none rounded-lg border border-pink-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-blue-900 focus:border-green-300 focus:outline-none focus:ring-0 "
       placeholder=" "
       {...props}
     />
